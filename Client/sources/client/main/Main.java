0565784c588157dc45e12d03a96a85ef46ea6d7f
ondal1997 <ondal1997@gmail.com>
2020-04-30 17:13:40 +0900

    Test commit

--- Client/sources/client/main/Main.java
+++ Client/sources/client/main/Main.java
@@ -3,7 +3,10 @@ package client.main;
 public class Main {
     public static void main(String[] args)
     {
+        Temp temp = new Temp();
+
         System.out.println("Hello, Client");
         System.out.println("Test");
+        System.out.println("Haha");
     }
 }
