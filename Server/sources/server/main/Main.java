435414634c2adbe1f9cf46eff8477fb394b8038f
ondal1997 <ondal1997@gmail.com>
2020-04-30 19:25:21 +0900

    Here we go

--- Server/sources/server/main/Main.java
+++ Server/sources/server/main/Main.java
@@ -3,21 +3,19 @@ package server.main;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Connection conn = null;
 
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/temp?useSSL=false", "root", "apmsetup");
             System.out.println("정상적으로 연결되었습니다.");
-        } catch(SQLException e) {
-            System.err.println("con 오류:" + e.getMessage());
-            e.printStackTrace();
+        } catch (SQLException e) {
         }
 
         try {
-            if(conn != null)
+            if (conn != null)
                 conn.close();
-        } catch (SQLException e) {}
+        } catch (SQLException e) {
+        }
     }
 }
