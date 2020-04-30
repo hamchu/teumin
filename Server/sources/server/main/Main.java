f7f700ea32e2070bc3b51f34dd6dab51f3192423
ondal1997 <ondal1997@gmail.com>
2020-04-30 18:16:39 +0900

    Server - Add conn

--- Server/sources/server/main/Main.java
+++ Server/sources/server/main/Main.java
@@ -1,8 +1,23 @@
 package server.main;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello, Server");
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/temp?useSSL=false", "root", "apmsetup");
+            System.out.println("정상적으로 연결되었습니다.");
+        } catch(SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            if(conn != null)
+                conn.close();
+        } catch (SQLException e) {}
     }
 }
