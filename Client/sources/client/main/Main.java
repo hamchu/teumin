d12c332a9421af4a7ed25b4e7a4abdb3f83160d6
ondal1997 <ondal1997@gmail.com>
2020-04-30 17:40:43 +0900

    Here is the starting point

--- Client/sources/client/main/Main.java
+++ Client/sources/client/main/Main.java
@@ -3,10 +3,6 @@ package client.main;
 public class Main {
     public static void main(String[] args)
     {
-        Temp temp = new Temp();
-
         System.out.println("Hello, Client");
-        System.out.println("Test");
-        System.out.println("Haha");
     }
 }
