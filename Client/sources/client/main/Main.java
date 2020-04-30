438d9fe05a701bb9b656be2f816153d212322a0a
ondal1997 <ondal1997@gmail.com>
2020-04-30 17:51:31 +0900

    Client - Add view

--- Client/sources/client/main/Main.java
+++ Client/sources/client/main/Main.java
@@ -1,8 +1,22 @@
 package client.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
+        primaryStage.setTitle("Hello, Client");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello, Client");
+        launch(args);
     }
 }
