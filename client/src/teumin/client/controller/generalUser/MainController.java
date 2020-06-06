fedc6b4680724b1201b126803f7ddcfab5d23bd6
ondal1997 <ondal1997@gmail.com>
2020-06-07 00:41:26 +0900

    Update [ General User Main ]

--- client/src/teumin/client/controller/generalUser/MainController.java
+++ client/src/teumin/client/controller/generalUser/MainController.java
@@ -1,10 +1,12 @@
 package teumin.client.controller.generalUser;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import teumin.client.Client;
 import teumin.client.util.addressQueryWindow.AddressQueryWindow;
+import teumin.client.util.categoryPickWindow.CategoryPickWindow;
 import teumin.entity.Address;
 import teumin.entity.Truck;
 import teumin.network.Data;
@@ -12,8 +14,39 @@ import teumin.network.Data;
 public class MainController extends Client {
 
     @FXML
-    void click_testAddressQueryWindow(MouseEvent event) throws Exception {
-        Address address = new AddressQueryWindow().showAndGet();
+    private TextField text_addressName;
+
+    @FXML
+    private TextField text_category;
+
+    private Address address;
+    private String category;
+
+    @FXML
+    void click_getAddress(MouseEvent event) throws Exception {
+
+        Address temp = new AddressQueryWindow().showAndGet();
+        if (temp != null) {
+            address = temp;
+            text_addressName.setText(address.getName());
+        }
+
+    }
+
+    @FXML
+    void click_getCategory(MouseEvent event) {
+
+        String temp = new CategoryPickWindow().showAndGet();
+        if (temp != null) {
+            category = temp;
+            text_category.setText(category);
+        }
+
+    }
+
+    @FXML
+    void click_queryTrucks(MouseEvent event) {
+
     }
 
 }
